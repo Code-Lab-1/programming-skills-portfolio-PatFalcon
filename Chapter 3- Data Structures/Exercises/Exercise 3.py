555d303f7d7bb76a10c6a4f458b9669b2186bbe4
Patrick Falcon <johnpatrick.falcon@gmail.com>
2022-11-03 13:51:38 +0800

    Create Exercise 3.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise 3.py
@@ -0,0 +1,5 @@
+favorite_transportation = ['car', "airplane", "motorcycle"]
+
+print("I go to work using {}".format(favorite_transportation[0]))
+print("I travel overseas using {}".format(favorite_transportation[1]))
+print("I travel to BathSpa using {}".format(favorite_transportation[2]))
